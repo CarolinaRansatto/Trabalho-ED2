6f6b7eb007acb01ee0ec31dfa4ad16243137007f
Carolina Ransatto <carolinaransatto@id.uff.br>
2016-12-17 01:43:10 -0200

    Libera e inserção em batch resolvidos, insere e imprime feitos

--- arvore.c
+++ arvore.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "arvore.h"
 
 TABM* inicializa() {
@@ -6,9 +9,14 @@ TABM* inicializa() {
 
 TABM* cria_no(int t) {
     TABM* a = (TABM*) malloc(sizeof(TABM));
-    a->alunos = (TA**) malloc(sizeof(TA*) * (2 * t - 1)); // deveria deixar nulo quando virar nó interno?
+    a->alunos = (TA**) malloc(sizeof(TA*) * (2 * t - 1));
+    int i;
+    for (i = 0; i < 2 * t - 1; i++)
+        a->alunos[i] = NULL;
     a->ant = NULL;
     a->filhos = (TABM**) malloc(sizeof(TABM*) * (2 * t));
+    for (i = 0; i < 2 * t; i++)
+        a->filhos[i] = NULL;
     a->folha = 1;
     a->mats = (int*) malloc(sizeof(int) * (2 * t - 1));
     a->nmats = 0;
@@ -16,7 +24,15 @@ TABM* cria_no(int t) {
     return a;
 }
 
-TA* cria_aluno(int mat, int chcs, int npu, int ntran, char *nome, float cr, int curriculo){
+TC* cria_curriculo(int cht, int ntotper, int tnc) {
+    TC* cur = (TC*) malloc(sizeof(TC));
+    cur->cht = cht;
+    cur->ntotper = ntotper;
+    cur->tnc = tnc;
+    return cur;
+}
+
+TA* cria_aluno(int mat, int chcs, int npu, int ntran, char *nome, float cr, TC* curriculo){
     TA* aluno = (TA*) malloc(sizeof(TA));
     aluno->mat = mat;
     aluno->chcs = chcs;
@@ -24,40 +40,92 @@ TA* cria_aluno(int mat, int chcs, int npu, int ntran, char *nome, float cr, int 
     aluno->ntran = ntran;
     strcpy(aluno->nome, nome);
     aluno->cr = cr;
-    //faz selecao dos curriculos
-    TC* cur = (TC*) malloc(sizeof(TC));
-    switch(curriculo){
-        case 1:
-            cur->cht = 2955;
-            cur->ntotper = 16;
-            cur->tnc = 8;
-            break;
-        case 2:
-            cur->cht = 3524;
-            cur->ntotper = 12;
-            cur->tnc = 8;
-            break;
-        case 3:
-            cur->cht = 3200;
-            cur->ntotper = 12;
-            cur->tnc = 8;
-            break;
-        default:
-            printf("Valor de curriculo invalido!");
-            exit(1);
-    }
-    aluno->cur = cur;
+    aluno->cur = curriculo;
     return aluno;
+}
+
+TABM* divisao(TABM* x, int i, TABM* y, int t) {
+    TABM* z = cria_no(t);
+    z->folha = y->folha;
+    int j;
+
+    if (z->folha) {
+        z->nmats = t; // vai ter mais chaves no nó da direita
+        for (j = 0; j < t; j++) {
+            z->mats[j] = y->mats[j + t - 1];
+            z->alunos[j] = y->alunos[j + t - 1];
+        }
+        z->prox = y->prox;
+        y->prox = z;
+        z->ant = y;
+    }
+    else { // pra nó interno funciona como árvore b
+        z->nmats = t - 1;
+        for (j = 0; j < t - 1; j++)
+            z->mats[j] = y->mats[j + t];
+        for (j = 0; j < t; j++) {
+            z->filhos[j] = y->filhos[j + t];
+            y->filhos[j + t] = NULL;
+        }
+    }
+
+    y->nmats = t - 1;
+    for (j = x->nmats; j >= i; j--) {
+        x->filhos[j + 1] = x->filhos[j];
+        x->mats[j] = x->mats[j - 1];
+    }
+    x->filhos[i] = z;
+    x->mats[i - 1] = y->mats[t - 1]; // igual à primeira matrícula de z se z for folha, "promovida" se z for nó interno
+    ++x->nmats;
+    return x;
+}
+
+TABM* insere_nao_completo(TABM* a, TA* aln, int t) {
+    int i = a->nmats - 1;
+    if (a->folha) {
+        while ((i >= 0) && (aln->mat < a->mats[i])) {
+            a->mats[i + 1] = a->mats[i];
+            a->alunos[i + 1] = a->alunos[i];
+            --i;
+        }
+        a->mats[i + 1] = aln->mat;
+        a->alunos[i + 1] = aln;
+        ++a->nmats;
+    }
+    else {
+        while ((i >= 0) && (aln->mat < a->mats[i])) i--;
+        ++i;
+        if (a->filhos[i]->nmats == 2 * t - 1) {
+            a = divisao(a, i + 1, a->filhos[i], t);
+            if (aln->mat > a->mats[i]) ++i;
+        }
+        a->filhos[i] = insere_nao_completo(a->filhos[i], aln, t);
+    }
+    return a;
 }
 
 TABM* insere(TABM* a, TA* aln, int t) {
     if (!a) {
         a = cria_no(t);
         a->alunos[0] = aln;
+        a->mats[0] = aln->mat;
+        a->prox = NULL;
+        a->ant = NULL;
         ++a->nmats;
         return a;
     }
+    if (a->nmats == (2 * t - 1)) {
+        TABM* s = cria_no(t);
+        s->folha = 0;
+        s->filhos[0] = a;
+        s = divisao(s, 1, a, t);
+        s = insere_nao_completo(s, aln, t);
+        return s;
+    }
+    a = insere_nao_completo(a, aln, t);
+    return a;
 }
+
 TABM* retira(TABM* a, int mat, int t);
 
 TA* busca(TABM* a, int mat) {
@@ -73,8 +141,6 @@ TA* busca(TABM* a, int mat) {
     return busca(a->filhos[i], mat);
 }
 
-//CRIAR METODO PARA LIBERAR ESTRUTURA TA
-/*
 void libera(TABM* a) {
     if (a) {
         int i;
@@ -83,15 +149,25 @@ void libera(TABM* a) {
                 libera(a->filhos[i]);
         }
         for (i = 0; i < a->nmats; i++)
-            libera(a->alunos[i]);
+            free(a->alunos[i]);
         free(a->alunos);
         free(a->filhos);
         free(a->mats);
         free(a);
     }
 }
-*/
-void imprime(TABM* a);
+
+void imprime(TABM *a, int andar){
+  if(a){
+    int i, j;
+    for(i = 0; i <= a->nmats-1; i++){
+      imprime(a->filhos[i], andar + 1);
+      for(j = 0; j <= andar; j++) printf("   ");
+      printf("%d\n", a->mats[i]);
+    }
+    imprime(a->filhos[i], andar+1);
+  }
+}
 
 void altera_chcs(TABM* a, int mat, int chcs) {
     TA* aln = busca(a, mat);
--- main.c
+++ main.c
@@ -5,30 +5,53 @@
 int main()
 {
     int opcao = 0;
-    printf("1 para inserir, -1 para sair\n");
+    printf("1 para inserir, 2 para imprimir, -1 para sair\n");
     scanf("%d",&opcao);
     int mat,chcs,npu,ntran,cur;
     float cr;
     char nome[51];
-    TA* aluno = (TA*) malloc(sizeof(TA));
+    TA* aluno;
+    TABM* arvore = inicializa();
+
+    TC* curs[3];
+    curs[0] = cria_curriculo(2955, 16, 8);
+    curs[1] = cria_curriculo(3524, 12, 8);
+    curs[2] = cria_curriculo(3200, 12, 8);
+
     while(opcao != -1){
         switch(opcao){
 
             case 1:
-                printf("Insira a matricula, cargas horarias cursadas com sucesso, numero de periodos cursados, trancamentos, cr, nome e numero do curriculo\n");
-                scanf("%d %f %d %d %d %d %[^\n]%*c", &mat,&cr,&ntran,&chcs,&npu,&cur,&nome);
-                aluno = cria_aluno(mat, chcs, npu, ntran, nome, cr, cur);
+                printf("Insira a matricula, cr, numero de trancamentos, cargas horarias cursadas com sucesso, numero de periodos na universidade, numero do curriculo e nome\n");
+                printf("Digite -1 para parar de inserir\n");
+                scanf("%d", &mat);
+                while (mat != -1) {
+                    scanf("%f %d %d %d %d %[^\n]%*c", &cr,&ntran,&chcs,&npu,&cur,&nome);
+                    aluno = cria_aluno(mat, chcs, npu, ntran, nome, cr, curs[cur - 1]);
+                    arvore = insere(arvore, aluno, 2);
+                    printf("%d %f %s\n", aluno->mat, aluno->cr, aluno->nome);
+                    scanf("%d", &mat);
+                }
                 //printf("%d %f %d %d %d %d %s\n", mat,cr,ntran,chcs,npu,cur,nome);
-                printf("%d %f %s", aluno->mat, aluno->cr, aluno->nome);
-                printf("1 para inserir, -1 para sair\n");
+                printf("1 para inserir, 2 para imprimir, -1 para sair\n");
+                scanf("%d",&opcao);
+                break;
+            case 2:
+                imprime(arvore, 0);
+                printf("1 para inserir, 2 para imprimir, -1 para sair\n");
                 scanf("%d",&opcao);
                 break;
             default:
                 printf("Opcao invalida!\n");
-                printf("1 para inserir, -1 para sair\n");
+                printf("1 para inserir, 2 para imprimir, -1 para sair\n");
                 scanf("%d",&opcao);
 
         }
     }
+
+    free(curs[0]);
+    free(curs[1]);
+    free(curs[2]);
+
     return 0;
 }
