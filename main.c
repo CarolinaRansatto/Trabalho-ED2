91a34b915e05b4f0ec67dee834ae431f2c1dcdc1
Carolina Ransatto <carolinaransatto@id.uff.br>
2016-12-17 13:28:07 -0200

    Busca no menu principal, verifica existência da matrícula antes de inserir

--- main.c
+++ main.c
@@ -2,14 +2,19 @@
 #include <stdlib.h>
 #include "arvore.c"
 
-#define T 2
-
 TABM* insere_arquivo(char* narq, TABM* a, int t, TC** curs);
 
 int main()
 {
-    int opcao = 0;
-    printf("1 para inserir manualmente, 2 para inserir de arquivo, 3 para remover, 4 para imprimir, 5 para remover formandos, 6 para remover os que violem a regra do 50%, 7 para remover os que violem a regra de tempo maximo, -1 para sair\n");
+    int opcao = 0, t = 0;
+    printf("Digite o valor de t: ");
+    scanf("%d", &t);
+    while (t < 2) {
+	    printf("Valor inválido (t deve ser maior que 1).\nDigite o valor de t: ");
+	    scanf("%d", &t);
+	}
+    
+    printf("1 para inserir manualmente, 2 para inserir de arquivo, 3 para buscar, 4 para remover, 5 para imprimir, 6 para remover formandos, 7 para remover os que violem a regra do 50%%, 8 para remover os que violem a regra de tempo maximo, -1 para sair\n");
     scanf("%d",&opcao);
     int mat,chcs,npu,ntran,cur;
     float cr;
@@ -26,57 +31,71 @@ int main()
         switch(opcao){
 
             case 1:
-                printf("Insira a matricula, cr, numero de trancamentos, cargas horarias cursadas com sucesso, numero de periodos na universidade, numero do curriculo e nome\n");
+                printf("Insira a matricula, cr, numero de trancamentos, carga horaria cursada com sucesso, numero de periodos na universidade, numero do curriculo e nome\n");
                 printf("Digite -1 para parar de inserir\n");
                 scanf("%d", &mat);
                 while (mat != -1) {
                     scanf("%f %d %d %d %d %[^\n]%*c", &cr,&ntran,&chcs,&npu,&cur,&nome);
-                    aluno = cria_aluno(mat, chcs, npu, ntran, nome, cr, curs[cur - 1]);
-                    arvore = insere(arvore, aluno, T);
+                    if ((cur > 3)||(cur < 1)) printf("Currículo %d inválido\n", cur);
+                    else if (busca_aluno(arvore, mat)) printf("Matrícula %d já cadastrada\n", mat);
+                    else {
+	                    aluno = cria_aluno(mat, chcs, npu, ntran, nome, cr, curs[cur - 1]);
+    	                arvore = insere(arvore, aluno, t);
+                    }
                     //printf("%d %f %s\n", aluno->mat, aluno->cr, aluno->nome);
                     scanf("%d", &mat);
                 }
                 //printf("%d %f %d %d %d %d %s\n", mat,cr,ntran,chcs,npu,cur,nome);
 
-			    printf("\n1 para inserir manualmente, 2 para inserir de arquivo, 3 para remover, 4 para imprimir, 5 para remover formandos, 6 para remover os que violem a regra do 50%, 7 para remover os que violem a regra de tempo maximo, -1 para sair\n");
+			    printf("\n1 para inserir manualmente, 2 para inserir de arquivo, 3 para buscar, 4 para remover, 5 para imprimir, 6 para remover formandos, 7 para remover os que violem a regra do 50%%, 8 para remover os que violem a regra de tempo maximo, -1 para sair\n");
                 scanf("%d",&opcao);
                 break;
 			case 2:
 				printf("Digite o nome do arquivo: ");
 				char narq[51];
 				scanf("%s", narq);
-				arvore = insere_arquivo(narq, arvore, T, curs);
-			    printf("\n1 para inserir manualmente, 2 para inserir de arquivo, 3 para remover, 4 para imprimir, 5 para remover formandos, 6 para remover os que violem a regra do 50%, 7 para remover os que violem a regra de tempo maximo, -1 para sair\n");
+				arvore = insere_arquivo(narq, arvore, t, curs);
+			    printf("\n1 para inserir manualmente, 2 para inserir de arquivo, 3 para buscar, 4 para remover, 5 para imprimir, 6 para remover formandos, 7 para remover os que violem a regra do 50%%, 8 para remover os que violem a regra de tempo maximo, -1 para sair\n");
                 scanf("%d",&opcao);
 				break;
-            case 3:
-				//remover
-			    printf("\n1 para inserir manualmente, 2 para inserir de arquivo, 3 para remover, 4 para imprimir, 5 para remover formandos, 6 para remover os que violem a regra do 50%, 7 para remover os que violem a regra de tempo maximo, -1 para sair\n");
+			case 3:
+				printf("Digite a matrícula a ser buscada: ");
+				scanf("%d", &mat);
+				aluno = busca_aluno(arvore, mat);
+				if (!aluno) printf("Aluno nao encontrado\n");
+				else printf("Nome: %s\nMatricula: %d\nCR:%f\nPeriodos na universidade: %d\nNumero de trancamentos: %d\nCarga horaria cursada com sucesso: %d\n", 
+					aluno->nome, aluno->mat, aluno->cr, aluno->npu, aluno->ntran, aluno->chcs);
+				printf("\n1 para inserir manualmente, 2 para inserir de arquivo, 3 para buscar, 4 para remover, 5 para imprimir, 6 para remover formandos, 7 para remover os que violem a regra do 50%%, 8 para remover os que violem a regra de tempo maximo, -1 para sair\n");
                 scanf("%d",&opcao);
-                break;
+				break;
             case 4:
-                imprime(arvore, 0);
-			    printf("\n1 para inserir manualmente, 2 para inserir de arquivo, 3 para remover, 4 para imprimir, 5 para remover formandos, 6 para remover os que violem a regra do 50%, 7 para remover os que violem a regra de tempo maximo, -1 para sair\n");
+				//remover
+			    printf("\n1 para inserir manualmente, 2 para inserir de arquivo, 3 para buscar, 4 para remover, 5 para imprimir, 6 para remover formandos, 7 para remover os que violem a regra do 50%%, 8 para remover os que violem a regra de tempo maximo, -1 para sair\n");
                 scanf("%d",&opcao);
                 break;
             case 5:
-                arvore = retira_formandos(arvore, 2);
-			    printf("\n1 para inserir manualmente, 2 para inserir de arquivo, 3 para remover, 4 para imprimir, 5 para remover formandos, 6 para remover os que violem a regra do 50%, 7 para remover os que violem a regra de tempo maximo, -1 para sair\n");
+                imprime(arvore, 0);
+			    printf("\n1 para inserir manualmente, 2 para inserir de arquivo, 3 para remover, 4 para imprimir, 5 para remover formandos, 6 para remover os que violem a regra do 50%%, 7 para remover os que violem a regra de tempo maximo, -1 para sair\n");
                 scanf("%d",&opcao);
                 break;
             case 6:
-                arvore = retira_alunos_tnc(arvore, 2);
-			    printf("\n1 para inserir manualmente, 2 para inserir de arquivo, 3 para remover, 4 para imprimir, 5 para remover formandos, 6 para remover os que violem a regra do 50%, 7 para remover os que violem a regra de tempo maximo, -1 para sair\n");
+                arvore = retira_formandos(arvore, t);
+			    printf("\n1 para inserir manualmente, 2 para inserir de arquivo, 3 para buscar, 4 para remover, 5 para imprimir, 6 para remover formandos, 7 para remover os que violem a regra do 50%%, 8 para remover os que violem a regra de tempo maximo, -1 para sair\n");
                 scanf("%d",&opcao);
                 break;
             case 7:
-                arvore = retira_alunos_ntotper(arvore, 2);
-			    printf("\n1 para inserir manualmente, 2 para inserir de arquivo, 3 para remover, 4 para imprimir, 5 para remover formandos, 6 para remover os que violem a regra do 50%, 7 para remover os que violem a regra de tempo maximo, -1 para sair\n");
+                arvore = retira_alunos_tnc(arvore, t);
+			    printf("\n1 para inserir manualmente, 2 para inserir de arquivo, 3 para buscar, 4 para remover, 5 para imprimir, 6 para remover formandos, 7 para remover os que violem a regra do 50%%, 8 para remover os que violem a regra de tempo maximo, -1 para sair\n");
+                scanf("%d",&opcao);
+                break;
+            case 8:
+                arvore = retira_alunos_ntotper(arvore, t);
+			    printf("\n1 para inserir manualmente, 2 para inserir de arquivo, 3 para buscar, 4 para remover, 5 para imprimir, 6 para remover formandos, 7 para remover os que violem a regra do 50%%, 8 para remover os que violem a regra de tempo maximo, -1 para sair\n");
                 scanf("%d",&opcao);
                 break;
             default:
                 printf("Opcao invalida!\n");
-			    printf("\n1 para inserir manualmente, 2 para inserir de arquivo, 3 para remover, 4 para imprimir, 5 para remover formandos, 6 para remover os que violem a regra do 50%, 7 para remover os que violem a regra de tempo maximo, -1 para sair\n");
+			    printf("\n1 para inserir manualmente, 2 para inserir de arquivo, 3 para buscar, 4 para remover, 5 para imprimir, 6 para remover formandos, 7 para remover os que violem a regra do 50%%, 8 para remover os que violem a regra de tempo maximo, -1 para sair\n");
                 scanf("%d",&opcao);
                 break;
         }
@@ -101,11 +120,14 @@ TABM* insere_arquivo(char* narq, TABM* a, int t, TC** curs) {
     char nome[51];
     TA* aluno;
 	int r = fscanf(fp, "%d %f %d %d %d %d %[^\n]%*c", &mat, &cr, &ntran, &chcs, &npu, &cur, &nome);
-	printf("%d", r);
 
 	while (r == 7) {
-		aluno = cria_aluno(mat, chcs, npu, ntran, nome, cr, curs[cur - 1]);
-		a = insere(a, aluno, t);
+        if ((cur > 3)||(cur < 1)) printf("Currículo %d inválido\n", cur);
+        else if (busca_aluno(a, mat)) printf("Matrícula %d já cadastrada\n", mat);
+        else {
+            aluno = cria_aluno(mat, chcs, npu, ntran, nome, cr, curs[cur - 1]);
+            a = insere(a, aluno, t);
+        }
 		r = fscanf(fp, "%d %f %d %d %d %d %[^\n]%*c", &mat, &cr, &ntran, &chcs, &npu, &cur, &nome);
 	}
 	
